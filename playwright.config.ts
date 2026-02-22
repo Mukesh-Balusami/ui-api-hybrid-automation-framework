d18e271a3a7db0b1e1be90dcf3b388a6f61bfd64
Mukesh Kumar Balusami <mukeshkumarbalusami@outlook.com>
2026-02-22 11:27:58 +0530

    Trigger CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET || ''
+    },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
